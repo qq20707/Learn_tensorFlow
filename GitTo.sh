afd23eaa4530aa1bf37a1cb3c8bb9277a68d8efe
qq20707 <liuhuan1949@outlook.com>
2017-04-15 17:15:54 +0800

    python_learn

--- GitTo.sh
+++ GitTo.sh
@@ -2,7 +2,9 @@
 git -T git@github.com
 git init
 git add .
-git commit -m "tensorflow_nolinear"
+
+
+git commit -m "python_learn"
 git remote rm origin
 git remote add origin https://github.com/qq20707/Learn_tensorFlow.git
 git push origin master
